50def84fc8704501f5fba5f1cd24bc8fc189aca8
Sammed0211 <108929367+Sammed0211@users.noreply.github.com>
2022-07-27 16:16:47 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,24 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=81;
+		char c;
+		c = (char)i;
+		System.out.println("The(int) value 81 is converted into(char) is: "+c);
+		
+		double d = 323.142;
+		i = (int)d;
+		System.out.println("The (double) value 323.142 is converted into (int) is: "+i);
+		
+		float f = 72.38f;
+		i = (int)f;
+		system.out.println("the (float) value is converted into (int) is: "+i);
+		
+		byte b ;
+		b = (byte)d;
+		system.out.println("the (double) value is converted into (int) is: "+i);
+	}
+
+}
