4dccd0f94f864a95ee68c98deea2fd201595d6b7
Sammed0211 <108929367+Sammed0211@users.noreply.github.com>
2022-08-11 16:45:55 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+ public class ArrayListExample{  
+ public static void main(String args[]){  
+  ArrayList<String> list=new ArrayList<String>();
+      list.add("Mobile");
+      list.add("Head Phone");    
+      list.add("Pendrive");    
+      list.add("Tab");    
+        
+      System.out.println(list);  
+ }  
+}  
